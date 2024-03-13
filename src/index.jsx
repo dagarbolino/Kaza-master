57f65c403a97ca8662137c77c139582b28e5e679
alexandre dnk <dincq.alexandre@gmail.com>
2024-03-13 19:43:53 +0100

    Mise à jour des fichiers CSS et JSX

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 
-import './sass/style.css'
+import './style.css'
 
 
 import Home from './pages/home/Home.jsx'
